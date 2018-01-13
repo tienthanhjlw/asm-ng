c560a768642b0914552f6335f4a26237ccec90ae
Thanh <tienthanh.jlw@gmail.com>
2018-01-14 03:27:24 +0700

    add data binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  github = 'fa-github-square';
+  spinner = 'fa-spinner fa-spin';
   constructor() { }
 
   ngOnInit() {
