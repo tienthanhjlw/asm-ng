00a5ee835f2a482defb1ad7ce1d665b3b6be4269
Thanh <tienthanh.jlw@gmail.com>
2018-01-14 00:57:05 +0700

    generate header, home and social components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { SocialComponent } from './social/social.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule
